04cb46b0feee9232f1675c8aabc7f1c03bc7738d
Angel Garcia <angel_dogui@hotmail.com>
2022-06-17 00:54:09 -0500

    9. Palindrome Number: 282 ms, 50.7MB

--- 9. Palindrome Number/index.js
+++ 9. Palindrome Number/index.js
@@ -3,7 +3,19 @@
  * @return {boolean}
  */
 var isPalindrome = function (x) {
-    
+    var digits = split(x);
+    if (digits[0] < 0) {
+        return false;
+    }
+    for (let i = 0; i < digits.length; i++) {
+        if (digits[i] !== digits.pop()) {
+            return false;
+        }
+    }
+    return true;
 };
 
-isPalindrome(121);
+const split = (n) =>
+    n < 10 ?
+        [n] :
+        [...split(Math.floor(n / 10)), n % 10]
